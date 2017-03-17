b5a450572074bc81c4975ff5f28def2a7131249d
Nick Sorrell <nick@cint.io>
2017-03-16 23:15:45 -0400

    Added word completion, exit on escape, home/end

--- InteractivePrompt/InteractivePrompt.cs
+++ InteractivePrompt/InteractivePrompt.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Cintio
@@ -20,24 +21,34 @@ namespace Cintio
             Console.Write(String.Concat(input));
             Console.SetCursorPosition(inputPosition + _prompt.Length, Console.CursorTop);
         }
+        private static IEnumerable<string> GetMatch(List<string> s, string input)
+        {
+            s.Add(input);
+            for (int i = 0; i < s.Count; i = (i+1)%s.Count)
+                if (Regex.IsMatch(s[i], ".*(?:" + input + ").*", RegexOptions.IgnoreCase))
+                    yield return s[i];
+        }
         /// <summary>
         /// Run will start an interactive prompt
         /// </summary>
         /// <param name="lambda">This func is provided for the user to handle the input.  Input is provided in both string and List&lt;char&gt;. A return response is provided as a string.</param>
         /// <param name="prompt">The prompt for the interactive shell</param>
         /// <param name="startupMsg">Startup msg to display to user</param>
-        public static void Run(Func<string, List<char>, string> lambda, string prompt, string startupMsg)
+        public static void Run(Func<string, List<char>, string> lambda, string prompt, string startupMsg, List<string> completionList = null)
         {
             _prompt = prompt;
             Console.WriteLine(startupMsg);
             List<List<char>> inputHistory = new List<List<char>>();
+            IEnumerator<string> wordIterator = null;
+            
             while (true)
             {
+                string completion = null;
                 List<char> input = new List<char>();
                 int inputPosition = 0;
                 int inputHistoryPosition = inputHistory.Count;
 
-                ConsoleKeyInfo key;
+                ConsoleKeyInfo key, lastKey = new ConsoleKeyInfo();
                 Console.Write(prompt);
                 do
                 {
@@ -58,6 +69,81 @@ namespace Cintio
                             Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
                         }
                     }
+
+                    else if (key.Key == ConsoleKey.Tab && completionList != null && completionList.Count > 0)
+                    {
+                        int tempPosition = inputPosition;
+                        List<char> word = new List<char>();
+                        while (tempPosition-- > 0 && !string.IsNullOrWhiteSpace(input[tempPosition].ToString()))
+                            word.Insert(0, input[tempPosition]);
+
+                        if (lastKey.Key == ConsoleKey.Tab)
+                        {
+                            wordIterator.MoveNext();
+                            if (completion != null)
+                            {
+                                ClearLine(input);
+                                for (var i = 0; i < completion.Length; i++)
+                                {
+                                    input.RemoveAt(--inputPosition);
+                                }
+                                RewriteLine(input, inputPosition);
+                            }
+                            else
+                            {
+                                ClearLine(input);
+                                for (var i = 0; i < string.Concat(word).Length; i++)
+                                {
+                                    input.RemoveAt(--inputPosition);
+                                }
+                                RewriteLine(input, inputPosition);
+                            }
+                        }
+                        else
+                        {
+                            ClearLine(input);
+                            for (var i = 0; i < string.Concat(word).Length; i++)
+                            {
+                                input.RemoveAt(--inputPosition);
+                            }
+                            RewriteLine(input, inputPosition);
+                            wordIterator = GetMatch(completionList, string.Concat(word)).GetEnumerator();
+                            while (wordIterator.Current == null)
+                                wordIterator.MoveNext();
+                        }
+
+                        completion = wordIterator.Current;
+                        ClearLine(input);
+                        foreach (var c in completion.ToCharArray())
+                        {
+                            input.Insert(inputPosition++, c);
+                        }
+                        RewriteLine(input, inputPosition);
+
+                    }
+                    else if (key.Key == ConsoleKey.Home || (key.Key == ConsoleKey.H && key.Modifiers == ConsoleModifiers.Control))
+                    {
+                        Console.WriteLine("crap");
+                        inputPosition = 0;
+                        Console.SetCursorPosition(prompt.Length, Console.CursorTop);
+                    }
+
+                    else if (key.Key == ConsoleKey.End || (key.Key == ConsoleKey.E && key.Modifiers == ConsoleModifiers.Control))
+                    {
+                        inputPosition = input.Count;
+                        Console.SetCursorPosition(inputPosition + _prompt.Length, Console.CursorTop);
+                    }
+
+                    else if (key.Key == ConsoleKey.Delete)
+                    {
+                        if (inputPosition < input.Count)
+                        {
+                            input.RemoveAt(inputPosition);
+                            ClearLine(input);
+                            RewriteLine(input, inputPosition);
+                        }
+                    }
+
                     else if (key.Key == ConsoleKey.UpArrow)
                     {
                         if (inputHistoryPosition > 0)
@@ -103,12 +189,22 @@ namespace Cintio
                         }
                     }
 
+                    else if (key.Key == ConsoleKey.Escape)
+                    {
+                        if (lastKey.Key == ConsoleKey.Escape)
+                            Environment.Exit(0);
+                        else
+                            Console.WriteLine("Press Escape again to exit.");
+                    }
+
                     else if (key.Key != ConsoleKey.Enter)
                     {
                         input.Insert(inputPosition++, key.KeyChar);
                         RewriteLine(input, inputPosition);
                     }
 
+
+                    lastKey = key;
                 } while (key.Key != ConsoleKey.Enter);
 
                 Console.WriteLine();
